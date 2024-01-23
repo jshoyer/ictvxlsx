c7514c543c052e28d88bcb527a83c5832f52a942
J. Steen Hoyer <j.s.hoyer@wustl.edu>
2024-01-23 18:43:11 -0500

    Shell script to thin VMR files; download all checkbox

---
+++ inst/extdata/winnow-away-minor-update-ICTV-MSL-files.sh
@@ -0,0 +1,8 @@
+
+# Redundant minor update files from ictv.global/msl (../../ReadMe)
+
+rm ICTV_Master_Species_List_2018a.xlsx \
+ICTV_Master_Species_List_2021_v1.xlsx \
+ICTV_Master_Species_List_2021_v2.xlsx \
+ICTV_Master_Species_List_2022_MSL38.v1.xlsx \
+ICTV_Master_Species_List_2022_MSL38.v2.xlsx \
