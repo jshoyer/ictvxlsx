1cd82415f40ee0d18426cc6cc94e7a5d9c1d92a7
J. Steen Hoyer <j.s.hoyer@wustl.edu>
2024-01-27 16:20:11 -0500

    Two major releases for 2018, MSL #33 and 34

--- inst/extdata/winnow-away-minor-update-ICTV-MSL-files.sh
+++ inst/extdata/winnow-away-minor-update-ICTV-MSL-files.sh
@@ -1,6 +1,8 @@
 
 # Redundant minor update files from ictv.global/msl (../../ReadMe)
 
+# Two major releases for 2018:
+# MSL #33 is 2018a, MSL #34 2018b (MSL #32 is 2017, MSL #35 is 2019)
 rm ICTV_Master_Species_List_2018a.xlsx \
 ICTV_Master_Species_List_2021_v1.xlsx \
 ICTV_Master_Species_List_2021_v2.xlsx \
