f35867908f486c26a92fb4dd9e20261c88d91a77
J. Steen Hoyer <j.s.hoyer@wustl.edu>
2024-06-07 13:27:19 -0400

    Example of brittle filtering by matching a text string

--- inst/extdata/winnow-away-minor-update-ICTV-MSL-files.sh
+++ inst/extdata/winnow-away-minor-update-ICTV-MSL-files.sh
@@ -8,3 +8,4 @@ ICTV_Master_Species_List_2021_v1.xlsx \
 ICTV_Master_Species_List_2021_v2.xlsx \
 ICTV_Master_Species_List_2022_MSL38.v1.xlsx \
 ICTV_Master_Species_List_2022_MSL38.v2.xlsx \
+ICTV_Master_Species_List_2023_MSL39.v1.xlsx \
